bedbf65341c37c11742afd30d6c9f4ecbfdfcc33
Jsin <Jsin1947@gmail.com>
2018-06-12 23:11:25 -0500

    Setup user input for the item list.

--- app/src/main/java/com/south42studio/jsin/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/south42studio/jsin/shoppinglist/ShoppingListActivity.java
@@ -2,6 +2,9 @@ package com.south42studio.jsin.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,6 +16,18 @@ public class ShoppingListActivity extends AppCompatActivity {
 
     private TextView usernameTv;
 
+    private Button addItemBttn;
+    private EditText itemNameEt;
+
+
+    private void assignViews() {
+        usernameTv = findViewById(R.id.username_tv);
+        addItemBttn = findViewById(R.id.add_item_bttn);
+        itemNameEt = findViewById(R.id.item_name_et);
+        usernameTv = findViewById(R.id.username_tv);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +37,8 @@ public class ShoppingListActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
-        usernameTv = findViewById(R.id.username_tv);
+        assignViews();
+        setOnclickListener();
 
         usernameTv.setText(currentUser.getEmail());
     }
@@ -32,5 +48,14 @@ public class ShoppingListActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         //TODO: check if user logged in then go to ListActivity
+    }
+
+    private void setOnclickListener(){
+        addItemBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usernameTv.setText(itemNameEt.getText().toString());
+            }
+        });
     }
 }
